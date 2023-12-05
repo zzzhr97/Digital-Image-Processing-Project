1bd1f3e4e05169681f0c16ea763fa480d95453ea
Zzzerough <3046769636@qq.com>
2023-12-05 20:56:19 +0800

    v0.5.2:简化评估并上传模型的步骤

--- README.md
+++ README.md
@@ -3,22 +3,20 @@
 
 ### 评估并上传模型步骤
 
-- 新建文件夹`upload_dir`
-- 将`model.py`，`eval_model.py`，`transform.py`，`data.py`，`utils.py`以及`metadata`放在`upload_dir`下
+- 由于`transform.py`可能不同，将自己的`transform.py`替换掉`upload_dir`中的`transform.py`文件
 - 将模型权重文件复制到`upload_dir`下，并改名成`Net.pth`
-- 打开`./upload_dir/model.py`文件，**注释掉`from network import *`这一行**，并设置参数:
-  - `net`: 存在于`network`文件夹下的模型类名，请**注释掉这一行**
+- 将网络定义文件复制到`upload_dir`下，并改名成`Net.py`
+- 打开`./upload_dir/model.py`文件，添加 **`from Net import xxx`** ，其中 **xxx** 是`Net.py`文件中的模型类名，并设置参数:
+  - `net`: **`Net.py`文件中的模型类名**
   - `num_classes`: 模型最后一层输出的形状，`1`或`2`
   - `ckpt_path`: 权重文件名，默认为`Net.pth`
   - `transform_method_origin`: 读取数据时所用的预处理方法编号
-- 打开`./upload_dir/model.py`文件，将需要使用的网络类复制到该文件中，替换掉`Net`类，然后在`model`类中的`load`函数里，将类名修改成需要使用的网络类，比如`self.model = TestNet(num_classes=num_classes)`
-- 打开`./upload_dir/eval_model.py`文件，将`ARGS`类中的`self.data_dir`改成`../dataset`
 - 运行命令
 ```bash
 python upload_dir/eval_model.py
 ```
 - 如果运行成功，将会输出模型在整个数据集的结果，否则就需要先 **debug**
-- 将`model.py`，`transform.py`，`Net.pth`，`metadata`四个文件打包成`upload_1.zip`，其中的`1`代表第1次提交
+- 将`model.py`，`transform.py`，`Net.py`，`Net.pth`，`metadata`五个文件打包成`upload_1.zip`，其中的`1`代表第1次提交
 - 打开[提交网站](https://codalab.lisn.upsaclay.fr/competitions/11877#participate)
 - 在 **Particapate** 中的 **Submit / View Results** 板块下，点击 **Future Test Phase - Task 1** ，进行提交（如果是task2，就选择 **Future Test Phase - Task 2**)
 
---
+++ upload_dir/transform.py
@@ -0,0 +1,78 @@
+import torchvision.transforms as transforms
+import torch
+import torch.nn.functional as F
+import cv2
+import random
+
+class transform_method:
+    def __init__(self, method=1):
+        self.transform1 = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        self.transform2 = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        self.method = method
+
+    def __call__(self, image):
+        """Return the transformation applied on the input image."""
+
+        # Example: 
+        # self.method = 1, then return self.method_1(image)
+        return getattr(self, f'method_{self.method}')(image)
+    
+    def normalization(self, iamge):
+        mean = torch.tensor([0.5, 0.5, 0.5])
+        std = torch.tensor([0.5, 0.5, 0.5])
+
+        image = F.normalize(image, mean, std)
+        return image
+    
+    """
+    transform_method_origin:在加载数据时,所有数据均会先经过这一method
+    transform_method_epoch:加载完毕数据之后,训练时的每一个epoch都会经过这一method
+
+    也就是说,train_data会经过transform_method_origin,然后经过transform_method_epoch
+    而valid_data只会经过transform_method_origin
+
+    所以,transform_method_epoch一般用于实现数据增强,比如随机裁剪,随机旋转等
+    而transform_method_origin一般用于实现数据预处理,比如resize,normalize等
+
+    如果不需要数据增强,则在命令行参数中令 transform_method_epoch = 0 即可
+    """
+
+    def method_0(self, image):
+        """Return the original image."""
+        return image
+
+    def method_1(self, image):
+        # resize image
+        image = cv2.resize(image, (512, 512)) # (800, 800, 3) -> (512, 512, 3)
+
+        # convert to tensor
+        image = torch.from_numpy(image).permute(2, 0, 1).to(torch.float)    # (3, 512, 512)
+
+        # normalize
+        image = image / 255.0
+
+        return image
+    
+    def method_2(self, image):
+        # input shape torch.tensor([3, x, x])
+        assert image.shape[0] == 3, f'Input image must have 3 channels, but got {image.shape[0]} channels.'
+
+        # torch.flip(image, [2]) 执行垂直翻转
+        # torch.flip(image, [1]) 执行水平翻转
+        # 即0.25的几率进行水平翻转,0.25的几率进行垂直翻转,0.5的几率不进行翻转
+        if random.random() < 0.25:
+            image = torch.flip(image, [1])
+        elif random.random() < 0.5:
+            image = torch.flip(image, [2])
+
+        return image
