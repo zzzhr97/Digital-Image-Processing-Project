def5ad2ecf36061962f9769314b5fbb24c120b47
Yaoyao Wang <91607859+llsune@users.noreply.github.com>
2023-12-11 21:10:20 +0800

    Update __init__.py

--- network/__init__.py
+++ network/__init__.py
@@ -1,7 +1,7 @@
 
 
 from .net_test import TestNet
-from .ResNet import resnet34, resnet50, resnet101
+from .ResNet import resnet18, resnet34, resnet50, resnet101
 from .DenseNet import DenseNet
 from .SResNet import SResNet
 from .AUNet import AUNet4, AUNet5, AUNet6
